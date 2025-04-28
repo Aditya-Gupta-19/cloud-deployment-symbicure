0fc046f6b0c0f6ecc4c7174ae0bbe64def19c726
Aditya-Gupta-19 <sendforaditya@gmail.com>
2025-04-29 01:23:12 +0530

    initial commit

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    apps:[
+        {
+    name:"Symbicure-final-main",
+    script:"npm",
+    arges:"un dev",
+    env:{
+        NODE_ENV:"development",
+        ENV_VAR1:"environment-variable",
+      },
+     },
+  ],
+};
